302f431086cb7bc2ca7aac2fb5eb1f92ea3b9754
apansukij <apisit.pa@lmwn.com>
2022-08-09 09:08:17 +0700

    add datatype learning script

---
+++ datatype.js
@@ -0,0 +1,16 @@
+/*
+Data types:
+undefined, boolean, string, object, number, null, and symbol
+*/
+var num = 10
+var str = 'hello'
+
+const pi = 3.14 // you can't change the constant
+
+let myhobby = 'reading books' // local variable
+
+// log result to the developer console
+console.log(num)
+console.log(str)
+console.log(pi)
+console.log(myhobby)
